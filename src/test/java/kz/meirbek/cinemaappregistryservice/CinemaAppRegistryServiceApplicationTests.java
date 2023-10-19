e6768727742d3b5349f1d8adf8f13de1ba09ebbd
Meirbek Ordaly <miko2329.Dev@gmail.com>
2023-10-19 15:10:32 +0600

    init commit

---
+++ src/test/java/kz/meirbek/cinemaappregistryservice/CinemaAppRegistryServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package kz.meirbek.cinemaappregistryservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CinemaAppRegistryServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
